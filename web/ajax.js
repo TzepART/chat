ae73cc0ae57af972f3b6a7acd956c20b6954e100
TzepART <artem060890@gmail.com>
2015-03-26 05:23:28 +0300

    Logic(step 4). Create AJAX query to update

--- web/ajax.js
+++ web/ajax.js
@@ -2,34 +2,71 @@
 /*
 Функция отправки AJAX запроса на сохранение и последующий вывод сообщения и никнейма
  */
-$('body').on("click","#btn_send",function save_massage()  //активизация запроса при клике по кнопке с id=btn_send
-    {
-            var user = $("#user_input").val();       //передача переменной user текста введенного в поле ввода никнейма
-            var message = $("#message_input").val(); //передача переменной message текста введенного в поле ввода сообщения
-            $.ajax({
-                metod: "POST",
-                url: "index.php?r=chat/save",       //url action контроллера, куда отправляется AJAX запрос
-                Datatype: "json",                   //тип данных возвращаемых на AJAX запрос
-                data: {                             //данные отправляемые AJAX запросом
-                    user: user,
-                    message: message
-                },
-                success: function (data) {          //в случае успешного AJAX запроса
-                    $('#user_input').val('');       //удаляется то что находится в поле ввода никнейма
-                    $('#message_input').val('');    //удаляется то что находится в поле ввода сообщения
-                    $('#user').empty();             //удаляется то что находится в поле вывода
-                    $('#message').empty();
-                    $('#time').empty();
-                    /*
-                    данные, в формате json, пришедших в ответ на запрос, вводятся в цикл для вывода
-                    в полях с соответствующими ID
-                     */
-                    for (var i = 0; i < data.length; i++) {
-                        $('#user').append($("<p>"+ data[i].user +"</p>"));
-                        $('#message').append($("<p>" + data[i].message+"</p>"));
-                        $('#time').append($("<p>"+ data[i].created_at +"</p>"));
+    $('body').on("click","#btn_send",function save_massage()  //активизация запроса при клике по кнопке с id=btn_send
+        {
+                var user = $("#user_input").val();       //передача переменной user текста введенного в поле ввода никнейма
+                var message = $("#message_input").val(); //передача переменной message текста введенного в поле ввода сообщения
+                $.ajax({
+                    metod: "POST",
+                    url: "index.php?r=chat/save",       //url action контроллера, куда отправляется AJAX запрос
+                    Datatype: "json",                   //тип данных возвращаемых на AJAX запрос
+                    data: {                             //данные отправляемые AJAX запросом
+                        user: user,
+                        message: message
+                    },
+                    success: function (data) {          //в случае успешного AJAX запроса
+                        $('#user_input').val('');       //удаляется то что находится в поле ввода никнейма
+                        $('#message_input').val('');    //удаляется то что находится в поле ввода сообщения
+                        $('#user').empty();             //удаляется то что находится в поле вывода
+                        $('#message').empty();
+                        $('#time').empty();
+                        /*
+                        данные, в формате json, пришедших в ответ на запрос, вводятся в цикл для вывода
+                        в полях с соответствующими ID
+                         */
+                        for (var i = 0; i < data.length; i++) {
+                            $('#user').append($("<p>"+ data[i].user +"</p>"));
+                            $('#message').append($("<p>" + data[i].message+"</p>"));
+                            $('#time').append($("<p>"+ data[i].created_at +"</p>"));
+                        }
                     }
+                });
+        }
+    );
+
+
+
+/*
+ Функция загрузки и обновления сообщений
+ */
+
+    function load_messages()
+    {
+        $.ajax({
+            metod: "POST",
+            url:  "index.php?r=chat/updatechat",        //url action контроллера, куда отправляется AJAX запрос
+            Datatype:"json",                            //тип данных возвращаемых на AJAX запрос
+            // Выводим то что вернул PHP
+            success: function(data)                     //в случае успешного AJAX запроса
+            {
+                $('#user').empty();         //удаляется то что находится в поле вывода
+                $('#message').empty();
+                $('#time').empty();
+                /*
+                 данные, в формате json, пришедших в ответ на запрос, вводятся в цикл для вывода
+                 в полях с соответствующими ID
+                 */
+                for (var i = 0; i < data.length; i++) {
+                    $('#user').append($("<p>"+ data[i].user +"</p>"));
+                    $('#message').append($("<p>" + data[i].message+"</p>"));
+                    $('#time').append($("<p>"+ data[i].created_at +"</p>"));
                 }
-            });
+            }
+        });
+
     }
-);
+
+//При загрузке страницы подгружаем сообщения
+load_messages();
+//Ставим цикл на каждые 10 секунды
+setInterval(load_messages,10000);
